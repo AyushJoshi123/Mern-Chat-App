a02db8a8ad6d1e91ba8cf59c870dc1c83ddfa277
AyushJoshi123 <joshiayush00712345@gmail.com>
2026-03-19 00:58:52 +0530

    authcontroller changed

--- server/models/user.js
+++ server/models/user.js
@@ -1,19 +1,29 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name:{
-    type:String,
-    required:true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
   },
-  email:{
-    type:String,
-    required:true,
-    unique:true
-  },
-  password:{
-    type:String,
-    required:true
-  }
-});
+  { timestamps: true }
+);
 
-export default mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
