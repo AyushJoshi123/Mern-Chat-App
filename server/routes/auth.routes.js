4cba0f85424f0805e6b01c4f3663fd5d060bcfce
AyushJoshi123 <joshiayush00712345@gmail.com>
2026-03-19 12:09:32 +0530

    installed libraries and updated controller

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -3,7 +3,16 @@ import {signup,signin,logout} from "../controller/auth.controller.js";
 import verifyToken from "../middleware/auth.middleware.js";
 
 const router = express.Router();
+// router.get("/signup",(req,res)=>{
+//     res.send("signup route")
+// })
 
+// router.get("signin" , (req,res)=>{
+//     res.send("signin route")
+// })
+// router.get("/logout", (req,res)=>{
+// res.send("logout route")
+// })
 router.post("/signup",signup);
 router.post("/signin",signin);
 router.post("/logout",verifyToken,logout);
