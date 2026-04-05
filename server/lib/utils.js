1f888bcc31df03df1246eac5b45fa3450f12b3ed
AyushJoshi123 <joshiayush00712345@gmail.com>
2026-04-05 10:26:56 +0530

    creating a live server link

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -16,7 +16,7 @@ export const generateToken = (userId, res) => {
     secure: process.env.NODE_ENV !== "development",
     sameSite: "strict",            // CSRF protection
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-  });
+  });// we are adding a comment 
 
   return token;
 };
