f0b91d1787a1443e1151743167c490cab5377114
AyushJoshi123 <joshiayush00712345@gmail.com>
2026-05-15 10:47:09 +0000

    atlas url added

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017");
+    const conn = await mongoose.connect(process.env.MONGODB);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
@@ -12,4 +15,3 @@ export const connectDB = async () => {
 };
 
 export default connectDB;
-
